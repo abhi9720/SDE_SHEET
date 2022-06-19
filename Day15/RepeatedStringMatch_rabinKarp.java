a1706919773599e6cdf1669e02fc92bbeaba460a
abhi9720 <abhishektiwari9720@gmail.com>
2022-06-19 16:24:27 +0530

    Day15

--- Day15/RepeatedStringMatch_rabinKarp.java
+++ Day15/RepeatedStringMatch_rabinKarp.java
@@ -1,8 +1,37 @@
-// Implementation 2
-// basic rabinkarp
+// Using built in function contain in java
+class Solution {
+    public int repeatedStringMatch(String s, String p) {        
+        int count = 1;
+        String src = s;
+        while(src.length() < p.length() ){
+            count+=1;
+            src+=s;
+        }        
+        if(src.contains(p) ) return count;
+        src+=s;
+        if(src.contains(p) ) return count+1;        
+        return -1;                
+    }   
+}
+
+// Using rabinkarp for pattern matching in string in O(n);
 class Solution {
 
-    public int strStr(String source, String target) {
+    public int repeatedStringMatch(String s, String p) {
+        
+        int count = 1;
+        String src = s;
+        while(src.length() < p.length() ){
+            count+=1;
+            src+=s;
+        }
+        if(src.equals(p) ) return count;
+        if(rabinkarp(src,p)!=-1 ) return count;        
+        if(rabinkarp(src+s,p)!=-1 ) return count+1;
+        return -1;                
+    }
+       
+    public static int rabinkarp(String source, String target) {
         int n = source.length();
         int m = target.length();
 
@@ -12,7 +41,7 @@ class Solution {
         long hTarget = 0; // hash of target
         long base = 31; // base
         long mod = 997; // hash table size , to handle very large number
-
+        
         // preCompute =  base^(N-1) need to remove out of window character
         // as we need to remove out of window digit we multiply it wit char*base^(N-1)
         long preCompute = 1;
@@ -29,19 +58,18 @@ class Solution {
         for (int j = m; j <= n; j++) {
             if (hSource == hTarget) {
                 if (source.substring(j - m, j).equals(target)) {
-                    return j - m;
+                    return j-m;
                 }
             }
 
             if (j < n) {
                 // this is where we are rolling our hash value to calculate next hashvlaue
-                // removed out of window character
-                hSource = (hSource+mod - (source.charAt(j - m) * preCompute)%mod) % mod;
-
+                // removed out of window character                
+                hSource = (hSource + mod - (source.charAt(j - m) * preCompute)%mod) % mod;
                 // add new character of this window
                 hSource = (hSource * base + source.charAt(j )) % mod;
             }
         }
         return -1;
-    }
+    }    
 }
